2032e8aa2381ca5d2819e4c44eecef5655ea6bf1
SamGRT <samantha.rojas@correo.unimet.edu.ve>
2025-06-16 20:33:39 -0400

    index sam

--- src/api/index.js
+++ src/api/index.js
@@ -27,8 +27,8 @@ app.use('/api/categorias', categoriasRoutes);
 
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  //useNewUrlParser: true,
+  //useUnifiedTopology: true
 })
 .then(() => console.log('Conectado a MongoDB'))
 .catch(err => console.error('Error al conectar MongoDB:', err));
