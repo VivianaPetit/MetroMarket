eef9d0b19ffb33404a0a9f7876c2beed5f1c691d
Viviii <145728669+VivianaPetit@users.noreply.github.com>
2025-06-16 20:01:12 -0400

    mejoras interfaz de perfil

--- src/app/(tabs)/menu.tsx
+++ src/app/(tabs)/menu.tsx
@@ -2,7 +2,6 @@ import { Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
 import React from 'react';
 import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { useAuth } from '../../context/userContext';
 
 export default function Perfil() {
