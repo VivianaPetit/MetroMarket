63ff016099f12d4f3841bdbe4fa1987951770f81
Buenaparte <131205534+Buenaparte@users.noreply.github.com>
2025-07-03 00:33:58 -0400

    Revert "diospt3"
    This reverts commit 41a1cc3266157616b21ac68f79256708ee558715.

--- src/app/Ranking.tsx
+++ src/app/Ranking.tsx
@@ -17,8 +17,8 @@ export default function Ranking() {
   const [promedios, setPromedios] = useState<Promedio[]>([]);
   const [listados, setlistados] = useState<string[]>([]);
   const [best_sellers, setBest_sellers] = useState<Usuario[]>([]);
-  const  [valor, setValor] = useState(0);
-  const  [valor2, setValor2] = useState(0);
+  const  [valor, setValor] = useState<number>(0);
+  var  valor2  = 0;
     const [permiso, setPermiso] = useState(true);
 
      useEffect(() => {
@@ -43,13 +43,13 @@ export default function Ranking() {
    const usuario_resenas = resenas.filter(pub => pub.resenado === id)
    if (usuario_resenas.length >= 1){ 
    const calculo = usuario_resenas.map((pub) => {
-      setValor2(valor2+ pub.calificacion)
+      valor2 = valor2+ pub.calificacion
    });
    if (valor2 >= valor){
     setValor(valor2)
     listados.push(id)
    } 
-   setValor2(0)
+   valor2 = 0
    }
  }
   
@@ -83,14 +83,26 @@ export default function Ranking() {
    const quitar_primero = categorias.map((bup) => {
     promedio(bup)
    });
+   var a = 1
    const agregar = promedios.map((bup) => {
-     console.log("a")
-     const encontrar = categorias.filter(pub => pub._id === bup._id)
+     if (bup.promedio < promedios[a].promedio && a <= promedios.length-1){ 
+     const encontrar = categorias.filter(pub => pub._id ===  promedios[a]._id)
+     console.log(encontrar)
      elegidos.push(encontrar[0])
+     const encontrar2 = categorias.filter(pub => pub._id === bup._id)
+     elegidos.push(encontrar2[0])
+     }else{ 
+       const encontrar = elegidos.filter(pub => pub._id === bup._id)
+       if (encontrar.length <= 0){
+             const encontrar = categorias.filter(pub => pub._id === bup._id)
+             elegidos.push(encontrar[0])
+       }
+     }
    });
+   
    setBest_sellers(elegidos)
    setPermiso(false)
-   console.log(best_sellers)
+   console.log(promedios)
   }
  }
 
@@ -98,44 +110,32 @@ export default function Ranking() {
    const usuario_resenas = resenas.filter(pub => pub.resenado === ususario._id)
    if (usuario_resenas.length >= 1){ 
    const calculo = usuario_resenas.map((pub) => {
-     let a = valor2 + pub.calificacion
-      setValor2(a)
+     valor2 = valor2 + pub.calificacion
+      //console.log(valor2)
    });
-   setValor2(valor2/usuario_resenas.length)
-   if (valor2 >= valor){
+   valor2 = valor2/usuario_resenas.length
+   console.log(valor2)
     setValor(valor2)
    const  promeidito = {
     _id: ususario._id,
-    promedio: valor2
-    }
+    promedio: valor2 }
     promedios.push(promeidito)
    } 
    }
- }
+
 
 return (
-  
-  <View style={styles.container}>
-  <ScrollView >
-    <View style={{flexDirection:'row',gap:15,alignItems:'center'}}>
-      <TouchableOpacity onPress={() => router.push('/menu')} >
-          <Ionicons name="arrow-back" size={24} color="#00318D" />
-    ` </TouchableOpacity>
-    <View ><Text style={styles.title}>RANKING BEST SELLER</Text></View>
-    </View>
-
-    <View>
-      <View style={styles.avatarContainer}>
-        <Image source={{ uri: vendedor?.foto }} style={styles.avatarImage} /> //*Aqui va la foto de solo el primer lugar
-        <Ionicons name='ribbon' size={60} style={{bottom:10}} ></Ionicons>
-      </View>
-      <Text style={styles.name}>PEPITA LUISA</Text> //*AQUI VA NOMBRE DEL PRIMER LUGAR
-      <Text style={styles.username}>samantha.rojas@correo.unimet.edu.ve</Text>//*AQUI VA correo
-      <Text style={styles.username}>04125678903</Text> //*AQUI VA telefono
-
-      
-        {best_sellers.map((pub) => (
-            <View style={styles.commentContainer}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.horizontalScroll}>
+            <View style={styles.productCardWrapper}>
+             <Text>Vendedor del mes</Text>
+              <Text>
+                  {vendedor?.nombre}
+                </Text>
+             <Text>{vendedor?.telefono}</Text>
+             <Text>{vendedor?.correo}</Text>
+             {best_sellers.map((pub) => (
+            <View style={styles.productCardWrapper}>
                 <Text>
                   {pub.nombre}
                 </Text>
@@ -143,17 +143,118 @@ return (
              <Text>{pub.correo}</Text>
             </View>
               ))}
-      
-      
+             <TouchableOpacity onPress={clasificacion} style={styles.backButton}>Ver vendedores del mes</TouchableOpacity>
+            </View>
+        </View>
+        </ScrollView>
+    );
+  };
 
-
-    </View>
-
-  </ScrollView>
-  </View>
-)
-
-      
+const styles = StyleSheet.create({
+  searchIcon: { marginRight: 8 },
+  searchInput: { flex: 1, fontSize: 14, color: '#333' },
+  categoriesWrapper: {
+    marginTop: 12,
+    paddingLeft: 16,
+  },
+  emptyContainer: {
+    alignItems: 'center',
+    marginTop: 50,
+    width: '100%',
+  },
+  emptyText: {
+    fontSize: 18,
+    color: '#888',
+    marginTop: 16,
+    textAlign: 'center',
+  },
+  sectionHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    marginBottom: 14,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333',
+    paddingHorizontal: 16,
+  },
+  seeAllText: {
+    color: '#00318D',
+    fontWeight: '500',
+  },
+  sponsoredSection: {
+    marginTop: 20,
+  },
+  sponsoredCard: {
+    backgroundColor: '#fff',
+    borderRadius: 12,
+    overflow: 'hidden',
+    elevation: 3,
+    marginRight: 10,
+    height: 200,
+  },
+  sponsoredBadge: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    backgroundColor: '#FF8C00',
+    paddingVertical: 3,
+    paddingHorizontal: 8,
+    borderRadius: 4,
+    zIndex: 2,
+  },
+  sponsoredBadgeText: {
+    color: '#fff',
+    fontSize: 10,
+    fontWeight: 'bold',
+  },
+  sponsoredImageContainer: {
+    width: '100%',
+    height: '70%',
+  },
+  sponsoredImage: {
+    width: '100%',
+    height: '100%',
+  },
+  sponsoredContent: {
+    padding: 10,
+  },
+  sponsoredTitle: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#333',
+  },
+  sponsoredPrice: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#00318D',
+    marginTop: 5,
+  },
+  categorySection: {
+    marginTop: 20,
+  },
+  horizontalScroll: {
+    flexDirection: 'row',
+    paddingHorizontal: 16,
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  horizontalProductCard: {
+    width: 160,
+    marginRight: 12,
+  },
+  productCardWrapper: {
+    width: '48%', // Ocupa casi la mitad del ancho (deja espacio para el margen)
+    marginBottom: 16, // Espacio vertical entre cards
+  },  
+  backButton: {
+    marginBottom: 10,
+    alignSelf: 'flex-start',
+  },
+});
 
 
 const styles = StyleSheet.create({
